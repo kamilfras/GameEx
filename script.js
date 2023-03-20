25b52a880c128f5f9cfa299f1459237377247031
Kamil Fras <37848845+kamilfras@users.noreply.github.com>
2023-03-20 23:24:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Your game logic goes here
-});
-
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 const tileSize = 50;
-const playerSprite = new Image();
-playerSprite.src = 'https://www.tibiawiki.com.br/images/7/76/Tibia_icon.png'; // Replace this with the URL of your player sprite image
+
+const playerSprites = {
+  up: new Image(),
+  right: new Image(),
+  down: new Image(),
+  left: new Image(),
+};
+
+playerSprites.up.src = 'https://www.tibiawiki.com.br/images/7/76/Tibia_icon.png';
+playerSprites.right.src = 'https://www.tibiawiki.com.br/images/7/76/Tibia_icon.png';
+playerSprites.down.src = 'https://www.tibiawiki.com.br/images/7/76/Tibia_icon.png';
+playerSprites.left.src = 'https://www.tibiawiki.com.br/images/7/76/Tibia_icon.png';
 
 const arrowIndicators = {
   up: document.querySelector('.arrow-indicator.up'),
@@ -18,24 +24,28 @@ const arrowIndicators = {
 let player = {
   x: 7,
   y: 7,
+  direction: 'down',
 };
 
-function drawTile(x, y, color) {
-  ctx.fillStyle = color;
+function drawTile(x, y, fillColor, strokeColor) {
+  ctx.fillStyle = fillColor;
   ctx.fillRect(x * tileSize, y * tileSize, tileSize, tileSize);
-}
-
-function drawPlayer(x, y) {
-  ctx.drawImage(playerSprite, x * tileSize, y * tileSize, tileSize, tileSize);
+  ctx.strokeStyle = strokeColor;
+  ctx.strokeRect(x * tileSize, y * tileSize, tileSize, tileSize);
 }
 
 function drawScene() {
   for (let x = 0; x < 15; x++) {
     for (let y = 0; y < 15; y++) {
-      drawTile(x, y, '#228B22');
+      drawTile(x, y, '#228B22', 'rgba(0, 0, 0, 0.1)');
     }
   }
-  drawPlayer(player.x, player.y);
+  drawPlayer(player.x, player.y, player.direction);
+}
+
+
+function drawPlayer(x, y, direction) {
+  ctx.drawImage(playerSprites[direction], x * tileSize, y * tileSize, tileSize, tileSize);
 }
 
 function updateScene() {
@@ -50,37 +60,42 @@ document.addEventListener('keydown', (e) => {
   switch (e.key) {
     case 'ArrowUp':
       dy = -1;
+      player.direction = 'up';
       break;
     case 'ArrowRight':
       dx = 1;
+      player.direction = 'right';
       break;
     case 'ArrowDown':
       dy = 1;
-break;
-case 'ArrowLeft':
-dx = -1;
-break;
-}
+      player.direction = 'down';
+      break;
+    case 'ArrowLeft':
+      dx = -1;
+      player.direction = 'left';
+      break;
+  }
 
-if (dx || dy) {
-player.x = Math.max(0, Math.min(14, player.x + dx));
-player.y = Math.max(0, Math.min(14, player.y + dy));
-updateScene();
-for (const direction in arrowIndicators) {
-  arrowIndicators[direction].style.backgroundColor =
-    direction.toLowerCase() === e.key.slice(5).toLowerCase() ? '#ffffff' : 'transparent';
-}
-}
+  if (dx || dy) {
+    player.x = Math.max(0, Math.min(14, player.x + dx));
+    player.y = Math.max(0, Math.min(14, player.y + dy));
+    updateScene();
+
+    for (const direction in arrowIndicators) {
+      arrowIndicators[direction].style.backgroundColor =
+        direction.toLowerCase() === e.key.slice(5).toLowerCase() ? '#ffffff' : 'transparent';
+    }
+  }
 });
 
 document.addEventListener('keyup', (e) => {
-if (['ArrowUp', 'ArrowRight', 'ArrowDown', 'ArrowLeft'].includes(e.key)) {
-for (const direction in arrowIndicators) {
-arrowIndicators[direction].style.backgroundColor = 'transparent';
-}
-}
+  if (['ArrowUp', 'ArrowRight', 'ArrowDown', 'ArrowLeft'].includes(e.key)) {
+    for (const direction in arrowIndicators) {
+      arrowIndicators[direction].style.backgroundColor = 'transparent';
+    }
+  }
 });
 
-playerSprite.onload = () => {
-drawScene();
+playerSprites.down.onload = () => {
+  drawScene();
 };
